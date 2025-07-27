de9dd96d8db30f1f0fdc3f37c8d5176433d4e495
a-dtya <adithyavinod1943@gmail.com>
2025-07-27 11:00:03 +0530

    init page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { HeroHeader } from "@/components/header";
 import HeroSection from "@/components/hero-section";
 
 export default function Home() {
   return (
-    <HeroSection />
+    <main>
+      <HeroHeader/>
+      <HeroSection />
+    </main>
+    
   );
 }
