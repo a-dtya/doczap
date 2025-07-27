42ad225cdfe34edf6606b5cf4925ff1731acd3f1
a-dtya <adithyavinod1943@gmail.com>
2025-07-27 18:50:25 +0530

    added server side signin signup

---
+++ server/users.ts
@@ -0,0 +1,37 @@
+"use server"
+
+import { auth } from "@/lib/auth";
+export const signInWithEmail = async(email:string,password:string)=>{
+
+    try{
+        const response = await auth.api.signInEmail({
+            body: {
+                email,
+                password
+            }
+        });
+
+        return {success:true,message:"User signed in successfully"}
+    }catch(error){
+        console.log("Error @server/users.ts signInWithEmail",error)
+        return {success:false,message:"Something went wrong"}
+    }   
+}
+
+export const signUpWithEmail = async(email:string,password:string,name:string)=>{
+    try{
+        const response = await auth.api.signUpEmail({
+            body: {
+                email,
+                password,
+                name
+            }
+        });
+
+        return {success:true,message:"User created and signed in successfully"}
+    }catch(error){
+        console.log("Error @server/users.ts signUpWithEmail",error)
+        return {success:false,message:"Something went wrong"}
+    }   
+}
+    
