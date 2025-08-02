61dc84cfca17c464835987aa798cf6764b7ed088
a-dtya <adithyavinod1943@gmail.com>
2025-08-02 16:15:17 +0530

    implemented editor save to backend

--- app/dashboard/document/[documentId]/page.tsx
+++ app/dashboard/document/[documentId]/page.tsx
@@ -5,6 +5,7 @@
 import { getDocumentById } from "@/server/docs"
 import { PageWrapper } from "@/components/page-wrapper"
 import RichTextEditor from "@/components/rich-text-editor"
+import { JSONContent } from "@tiptap/react"
 export default async function DocumentPage({ params }: { params: { documentId: string } }) {
     const documentId = params.documentId
     const document = await getDocumentById(documentId)
@@ -18,6 +19,6 @@ export default async function DocumentPage({ params }: { params: { documentId: s
             href: `/dashboard/document/${documentId}`,
         }
     ]}>{document.data?.title}
-    <RichTextEditor />
+    <RichTextEditor content={document.data?.content as JSONContent[]} documentId={documentId}/>
     </PageWrapper>
 }
--- components/rich-text-editor.tsx
+++ components/rich-text-editor.tsx
@@ -1,9 +1,10 @@
 
 
+import { updateDocument } from '@/server/docs'
 import '@styles/styles.scss'
 
 import { TextStyleKit } from '@tiptap/extension-text-style'
-import type { Editor } from '@tiptap/react'
+import type { Editor, JSONContent } from '@tiptap/react'
 import { EditorContent, useEditor, useEditorState } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import React from 'react'
@@ -154,44 +155,30 @@ function MenuBar({ editor }: { editor: Editor }) {
   )
 }
 
-export default function RichTextEditor() {
+interface RichTextEditorProps {
+    content?: JSONContent[]
+    documentId?: string
+}
+
+export default function RichTextEditor({ content, documentId }: RichTextEditorProps) {
   const editor = useEditor({
     extensions,
-    content: `
-<h2>
-  Hi there,
-</h2>
-<p>
-  this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-</p>
-<ul>
-  <li>
-    That’s a bullet list with one …
-  </li>
-  <li>
-    … or two list items.
-  </li>
-</ul>
-<p>
-  Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-</p>
-<pre><code class="language-css">body {
-  display: none;
-}</code></pre>
-<p>
-  I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-</p>
-<blockquote>
-  Wow, that’s amazing. Good work, boy! 👏
-  <br />
-  — Mom
-</blockquote>
-`,
+    autofocus: true,
+    editable: true,
+    injectCSS: false,
+    onUpdate: ({ editor }) => {
+      if (documentId) {
+        updateDocument(documentId, editor.getJSON())
+      }
+    },
+    content
   })
   return (
     <div>
       <MenuBar editor={editor} />
-      <EditorContent editor={editor} />
+      <div className="w-full max-w-7xl min-h-96 border border-gray-200 rounded-md p-4">
+        <EditorContent editor={editor} />
+      </div>
     </div>
   )
 }
